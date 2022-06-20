ab7f0fd34dd91341b8b9a88a117db9d1255bf28b
Saravanan M <sarvan.m20@gmail.com>
2022-06-20 11:53:46 +0530

    Navbar and header created

---
+++ Frontend/amazon/src/Components/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Header } from './Header'
+import { Navbar } from './Navbar'
+
+export const Homepage = () => {
+  return (
+    <div>
+        <h3>Home</h3>
+        <div><Navbar /></div>
+        <div><Header /></div>
+    </div>
+  )
+}
