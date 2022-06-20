919681327a92fb036da4affd32d3f21a736b4c45
Saravanan M <sarvan.m20@gmail.com>
2022-06-20 14:01:54 +0530

    Updated

--- Frontend/amazon/src/App.jsx
+++ Frontend/amazon/src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import { Homepage } from './Components/Homepage'
 
 function App() {
 
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='/' element={<Homepage />}/>
+        {/* <Route />
+        <Route />
+        <Route /> */}
+      </Routes>
     </div>
   )
 }
